45a16f230d32b3c607a4d2511a6a9e032db1f914
Sagyam Thapa <sagyamthapa32@gmail.com>
2023-11-27 08:59:21 +0545

    build(app): relaxed eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,9 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["sznm/react", "plugin:react/jsx-runtime"],
+  extends: [ 'plugin:@typescript-eslint/recommended','plugin:prettier/recommended', "plugin:react/jsx-runtime"],
   ignorePatterns: ["node_modules/", "dist/", "ui/", ".turbo/", "public/"],
   rules: {
     "@typescript-eslint/consistent-type-imports": "error",
     "react/hooks/exhaustive-deps": "off",
   }
-
 };
